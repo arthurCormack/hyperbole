0e94581bc3dfbcd2e5a106ee2ee585b8160ceff4
Arthur Cormack <a.cormack@zoomermedia.ca>
2017-12-18 17:01:45 -0500

    stubbed some wp theme and plugin files

---
+++ wp/plugins/hyperbole/hyperbole.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: Hyperbole
+* Plugin URI: none
+* Description: Custom JSON REST endpoints, an utility functions, for things like thumbnails, and wp hooks
+* Version: 0.2.3
+* Authors: Arthur Cormack
+* Author URI: none
+* License: none
+*/
