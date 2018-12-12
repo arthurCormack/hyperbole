d35e0ebe19d7afc9b7d7fce746a36705dd4b4e38
Arthur Cormack <a.cormack@zoomermedia.ca>
2018-12-12 16:42:37 -0500

    please add the damn spa now

---
+++ spa/app/containers/HomePage/selectors.js
@@ -0,0 +1,16 @@
+/**
+ * Homepage selectors
+ */
+
+import { createSelector } from 'reselect';
+import { initialState } from './reducer';
+
+const selectHome = state => state.get('home', initialState);
+
+const makeSelectUsername = () =>
+  createSelector(
+    selectHome,
+    homeState => homeState.get('username'),
+  );
+
+export { selectHome, makeSelectUsername };
