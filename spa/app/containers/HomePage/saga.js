1a14f3fe3d800f8cf8ee8fc10e8989068427a408
Arthur Cormack <a.cormack@zoomermedia.ca>
2018-12-21 14:07:15 -0500

    ... latest ... many questions to answer, about expected routes from wordpress, etc. Do we want to simply hard code some opinionated arbitrary routes, or come up with a way of discovering routes? opinionated for now. It means that the sagas in the spa need to be aware of the endpoints in the api

--- spa/app/containers/HomePage/saga.js
+++ spa/app/containers/HomePage/saga.js
@@ -6,6 +6,8 @@ import { call, put, select, takeLatest } from 'redux-saga/effects';
 // import { LOAD_REPOS } from 'containers/App/constants';
 // import { reposLoaded, repoLoadingError } from 'containers/App/actions';
 
+import { isServer } from 'utils/detection';
+
 import request from 'utils/request';
 // import { makeSelectUsername } from 'containers/HomePage/selectors';
 
@@ -36,3 +38,25 @@ import request from 'utils/request';
 //   // It will be cancelled automatically on component unmount
 //   yield takeLatest(LOAD_REPOS, getRepos);
 // }
+function* firstLoadSaga() {
+  // what is the request url ...
+  // this is an interesting and complex thing to resolve ... where / how to we call the api ? how is the api's endpoints going to be organized?
+  // do we simply pull the most recent posts? or do we make a special page that lets the site editor select articles? or a slideshow? in short, how do we know what we are doing here on the front page?
+  // initially, let's just keep it simple. Get the n latest posts.
+  // much of the gutenberg bells and whistles might not work, in dangerouslySetInnerHTML'd content; js that is supposed to be included in the head will not be, and the document.ready, came and went before the content got loaded.
+}
+
+export default function* homePageSaga() {
+  // why not, instead of making each one of these sagas check to see if it is on the clinet ort server, simply check here, and only call them if/when we need to. as opposed to always calling them, and leaving it up toi them wether or not they participate, based on whether they are on the client or server.
+
+
+  if (isServer()) {
+    yield firstLoadSaga();
+  } else {
+
+    yield firstLoadSaga();// we have to call it here, in case we are not arriving on this page, on first load
+    // any other sagas that we want to have happen on the home page.
+
+  }
+
+}
