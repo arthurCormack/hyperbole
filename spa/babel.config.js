cac884e539a6a2dee30a4dcd04a148167cbdbcb3
Arthur Cormack <a.cormack@zoomermedia.ca>
2019-04-18 15:43:02 -0400

    a ferw upgrades in the package.json

--- spa/babel.config.js
+++ spa/babel.config.js
@@ -1,37 +1,3 @@
-// module.exports = {
-//   presets: [
-//     [
-//       '@babel/preset-env',
-//       {
-//         modules: false,
-//       },
-//     ],
-//     '@babel/preset-react',
-//   ],
-//   plugins: [
-//     'styled-components',
-//     '@babel/plugin-proposal-class-properties',
-//     '@babel/plugin-syntax-dynamic-import',
-//   ],
-//   env: {
-//     production: {
-//       only: ['app'],
-//       plugins: [
-//         'lodash',
-//         'transform-react-remove-prop-types',
-//         '@babel/plugin-transform-react-inline-elements',
-//         '@babel/plugin-transform-react-constant-elements',
-//       ],
-//     },
-//     test: {
-//       plugins: [
-//         '@babel/plugin-transform-modules-commonjs',
-//         'dynamic-import-node',
-//       ],
-//     },
-//   },
-// };
-
 const loadableBabelPlugin = require('@loadable/babel-plugin');
 
 function isWebTarget(caller) {
