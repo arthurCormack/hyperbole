a1bbc939af813bcb106778ba4c35015bb1681cf3
Arthur Cormack <cormack.arthur@gmail.com>
2019-05-02 09:06:59 -0400

    ... sorting out the immer reducers

--- spa/app/containers/HomePage/actions.js
+++ spa/app/containers/HomePage/actions.js
@@ -15,18 +15,18 @@
  *    }
  */
 
-import { CHANGE_USERNAME } from './constants';
+// import { CHANGE_USERNAME } from './constants';
 
-/**
- * Changes the input field of the form
- *
- * @param  {string} username The new text of the input field
- *
- * @return {object} An action object with a type of CHANGE_USERNAME
- */
-export function changeUsername(username) {
-  return {
-    type: CHANGE_USERNAME,
-    username,
-  };
-}
+// /**
+//  * Changes the input field of the form
+//  *
+//  * @param  {string} username The new text of the input field
+//  *
+//  * @return {object} An action object with a type of CHANGE_USERNAME
+//  */
+// export function changeUsername(username) {
+//   return {
+//     type: CHANGE_USERNAME,
+//     username,
+//   };
+// }
--- spa/app/containers/HomePage/reducer.js
+++ spa/app/containers/HomePage/reducer.js
@@ -8,22 +8,94 @@
  */
 
 import produce from 'immer';
+
+import { combineReducers } from 'redux';
+
+import {
+  LOAD_HOMEPAGE_INITIALPOSTS,
+  LOAD_HOMEPAGE_INITIALPOSTS_SUCCESS,
+  LOAD_HOMEPAGE_INITIALPOSTS_FAILURE,
+} from './constants';
+
+// // The initial state of the initialPosts
+const initialPostsState = {
+  loading: false,
+  error: false,
+  posts: false,// false or []
+};
+
+// /* eslint-disable default-case, no-param-reassign */
+// const initialPosts = (state = initialPostsState, action) => {
+//   produce(state, draft => {
+//     switch(action.type) {
+//       case LOAD_HOMEPAGE_INITIALPOSTS:
+//         draft.loading = true;
+//         draft.error = false;
+//         draft.posts = false;
+//         break;
+//       case LOAD_HOMEPAGE_INITIALPOSTS_SUCCESS:
+//         draft.loading = false;
+//         draft.error = false;
+//         draft.posts = action.posts;
+//         break;
+//       case LOAD_HOMEPAGE_INITIALPOSTS_FAILURE:
+//         draft.loading = false;
+//         draft.error = true;
+//         draft.posts = false;
+//         break;
+//       default:
+//         draft = draft;
+//     }
+//   })
+// }
+
+export const initialState = {
+  initialPosts: initialPostsState,
+};
+// export const initialState = initialPostsState;
+// // const homeReducer = combineReducers({
+// //   // initialPosts,
+// // });
+
+// // export default homeReducer;
+// export default initialPosts;
+
+
 import { CHANGE_USERNAME } from './constants';
 
 // The initial state of the App
-export const initialState = {
-  username: '',
-};
+// export const initialState = {
+//   username: '',
+// };
 
 /* eslint-disable default-case, no-param-reassign */
-const homeReducer = (state = initialState, action) =>
+const initialPosts = (state = initialState, action) =>
   produce(state, draft => {
     switch (action.type) {
       case CHANGE_USERNAME:
         // Delete prefixed '@' from the github username
         draft.username = action.username.replace(/@/gi, '');
         break;
+      case LOAD_HOMEPAGE_INITIALPOSTS:
+        draft.loading = true;
+        draft.error = false;
+        draft.posts = false;
+        break;
+      case LOAD_HOMEPAGE_INITIALPOSTS_SUCCESS:
+        draft.loading = false;
+        draft.error = false;
+        draft.posts = action.posts;
+        break;
+      case LOAD_HOMEPAGE_INITIALPOSTS_FAILURE:
+        draft.loading = false;
+        draft.error = true;
+        draft.posts = false;
+        break;
     }
   });
+
+const homeReducer = combineReducers({
+  initialPosts,
+});
 
 export default homeReducer;
